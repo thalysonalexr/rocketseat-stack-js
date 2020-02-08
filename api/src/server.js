e96cf0e3451528c5fb9f43995ed77453ad3ac0d3
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-02-07 22:38:32 -0400

    update

--- api/src/server.js
+++ api/src/server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const router = require("./routes");
 
-const mongoUrl = "mongodb://localhost:27017/nodeapi";
+const mongoUrl = "mongodb://mongo:27017/nodeapi";
 
 mongoose.connect(mongoUrl, {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 }, () => console.log("Database is connected."));
 
 const app = express();
 
-app.use(router);
+app.use(cors());
+app.use(express.json());
+app.use("/v1", router);
 
 const port = process.env.PORT || 3000;
 const host = "0.0.0.0";
