1188e4b3b232303427ef9bc2859ee73cd2c12756
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-02-07 22:38:45 -0400

    finish crud

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const ProductControlller = require("./controllers/ProductController");
 
 const router = express.Router();
+
+router.get("/products", ProductControlller.index);
+router.get("/products/:id", ProductControlller.show);
+router.post("/products", ProductControlller.store);
+router.put("/products/:id", ProductControlller.update);
+router.delete("/products/:id", ProductControlller.destroy);
 
 module.exports = router;
